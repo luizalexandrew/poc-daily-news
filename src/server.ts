81d472d0cbff53c231cdaf092c2c6839d4c184e1
Luiz Alexandre de Sousa Freitas <luizalexandredev@gmail.com>
2024-08-17 11:42:07 -0300

    Publicando Story

--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,23 @@ require("dotenv").config();
 const { IgApiClient } = require("instagram-private-api");
 const { get } = require("request-promise");
 
-const CronJob = require("cron").CronJob;
+// import { StickerBuilder } from '../src/sticker-builder';
+
+
 
 const app = express();
 
 const port: number = 3000;
 
-const postToInsta = async () => {
-  const ig = new IgApiClient();
+const ig = new IgApiClient();
+
+async function login() {
   ig.state.generateDevice(process.env.IG_USERNAME);
   await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
+}
+
+const postToInstaImage = async () => {
+  await login();
 
   const imageBuffer = await get({
     url: "https://img.freepik.com/vetores-gratis/instagram-fundo-em-cores-gradientes_23-2147823814.jpg",
@@ -26,10 +33,109 @@ const postToInsta = async () => {
   });
 };
 
-app.get("/", (req: Request, res: Response) => {
-  postToInsta();
+const postToInstaStory = async () => {
 
-  res.send("Postando no instagram");
+    await login();
+    const file = await get({
+        url: "https://img.freepik.com/vetores-gratis/instagram-fundo-em-cores-gradientes_23-2147823814.jpg",
+        encoding: null,
+      });
+
+    await ig.publish.story({
+        file,
+        // this creates a new config
+        // stickerConfig: new StickerBuilder()
+        //   // these are all supported stickers
+        //   .add(
+        //     StickerBuilder.hashtag({
+        //       tagName: 'insta',
+        //     }).center(),
+        //   )
+        //   .add(
+        //     StickerBuilder.mention({
+        //       userId: ig.state.cookieUserId,
+        //     }).center(),
+        //   )
+        //   .add(
+        //     StickerBuilder.question({
+        //       question: 'My Question',
+        //     }).scale(0.5),
+        //   )
+        //   .add(
+        //     StickerBuilder.question({
+        //       question: 'Music?',
+        //       questionType: 'music',
+        //     }),
+        //   )
+        //   .add(
+        //     StickerBuilder.countdown({
+        //       text: 'My Countdown',
+        //       // @ts-ignore
+        //       endTs: DateTime.local().plus(Duration.fromObject({ hours: 1 })), // countdown finishes in 1h
+        //     }),
+        //   )
+        //   .add(
+        //     StickerBuilder.chat({
+        //       text: 'Chat name',
+        //     }),
+        //   )
+        //   .add(
+        //     StickerBuilder.location({
+        //       locationId: (await ig.locationSearch.index(13, 37)).venues[0].external_id,
+        //     }),
+        //   )
+        //   .add(
+        //     StickerBuilder.poll({
+        //       question: 'Question',
+        //       tallies: [{ text: 'Left' }, { text: 'Right' }],
+        //     }),
+        //   )
+        //   .add(
+        //     StickerBuilder.quiz({
+        //       question: 'Question',
+        //       options: ['0', '1', '2', '3'],
+        //       correctAnswer: 1,
+        //     }),
+        //   )
+        //   .add(
+        //     StickerBuilder.slider({
+        //       question: 'Question',
+        //       emoji: '❤',
+        //     }),
+        //   )
+    
+        //   // mention the first story item
+        //   .add(StickerBuilder.mentionReel((await ig.feed.userStory('username').items())[0]).center())
+    
+        //   // mention the first media on your timeline
+        //   .add(StickerBuilder.attachmentFromMedia((await ig.feed.timeline().items())[0]).center())
+    
+        //   // you can also set different values for the position and dimensions
+        //   .add(
+        //     StickerBuilder.hashtag({
+        //       tagName: 'insta',
+        //       width: 0.5,
+        //       height: 0.5,
+        //       x: 0.5,
+        //       y: 0.5,
+        //     }),
+        //   )
+        //   .build(),
+      });
+
+
+};
+
+app.get("/story", (req: Request, res: Response) => {
+  postToInstaStory();
+
+  res.send("Postando no story instagram");
+});
+
+app.get("/image", (req: Request, res: Response) => {
+  postToInstaImage();
+
+  res.send("Postando no imagem instagram");
 });
 
 app.listen(port, () => {
