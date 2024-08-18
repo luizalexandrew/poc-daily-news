47b6fdb3873069f2bf9cdbab5e86435692d83ead
Luiz Alexandre de Sousa Freitas <luizalexandredev@gmail.com>
2024-08-18 15:50:42 -0300

    Component story

--- src/GreetingsCard.tsx
+++ src/GreetingsCard.tsx
@@ -51,9 +51,13 @@ export class GreetingsCard extends Builder<Props> {
     const { type, displayName, avatar, message } = this.options.getOptions();
 
     // make sure to use the loadImage helper function to load images, otherwise you may get errors
-    const image = await loadImage(avatar);
+    const image = await loadImage("https://files.metropoles.com/static/expediente/assets/images/general/portal-metropoles.jpg");
     const post = await loadImage(
-      "https://blog.cielo.com.br/wp-content/uploads/2019/03/quais-sao-as-taxas-da-cielo.jpg"
+      "https://www.infomoney.com.br/wp-content/uploads/2024/06/2024-06-06T200313Z_1_LYNXMPEK550T9_RTROPTP_4_META-WHATSAPP-BRAZIL.jpg"
+    );
+
+    const logo = await loadImage(
+      "https://logodownload.org/wp-content/uploads/2019/09/infomoney-logo.png"
     );
 
     return (
@@ -80,37 +84,57 @@ export class GreetingsCard extends Builder<Props> {
         </div>
 
         <div className="flex flex-col w-[1080px] h-[1920px] z-10">
-          <div className="flex items-start p-10 bg-[#FFFF00] h-[170px]">
-            <img
-              src={image.toDataURL()}
-              className="flex h-[100px] w-[100px] rounded-full"
-            />
-            <div className="flex flex-col ml-6">
-              <h1 className="text-5xl text-white font-bold m-0">
-                {type === "welcome" ? "Welcome" : "Olocooo"},{" "}
-                <span className="text-blue-500">{displayName}!</span>
-              </h1>
-              <p className="text-gray-300 text-3xl m-0">{message}</p>
+
+          <div
+            className="flex flex-col bg-[#000000]"
+            style={{
+              opacity: 1,
+              bottom: 0,
+              left: 0,
+              display: 'flex',
+              position: 'absolute',
+              width: '100%',
+              WebkitMaskImage: 'linear-gradient(to top, rgba(0,0,0,1) 0%, rgba(0,0,0,0.90) 40%, rgba(0,0,0,0.5) 60%, rgba(0,0,0,0.0) 100%)',
+              // backgroundImage: 'linear-gradient(to left, rgba(0,0,0,1), rgba(0,0,0,0)), linear-gradient(to right, rgba(0,0,0,1), rgba(0,0,0,0))'
+            }}
+          >
+            <span className="pt-[100%] inline-block"></span>
+
+            <div className="flex items-start p-10 h-[170px]">
+              <img
+                src={image.toDataURL()}
+                className="flex h-[80px] w-[80px] rounded-full"
+              />
+              <div className="flex flex-col ml-6">
+                <h1 className="text-5xl text-white font-bold m-0">
+                  <span className="text-blue-500">{"Daily.fin"}!</span>
+                </h1>
+                <p className="text-gray-300 text-3xl m-0">{"Seu feed de notícias"}</p>
+              </div>
+            </div>
+
+
+            <h1 className="text-6xl text-white px-[20px] font-black">Dividendos da semana: Petrobras, Copasa e Localiza estão entre as empresas que pagam</h1>
+            <h3 className="text-4xl text-white px-[20px]">No total, cinco empresas distribuem dividendos e juros sobre capital próprio até a sexta-feira (23)</h3>
+            <h4 className="text-3xl text-white px-[20px]">18/08/2024 06h00 • Atualizado 2 dias atrás</h4>
+            <div className="flex items-center p-10 bg-[#30363d] rounded-3xl	 mb-[100px] ml-[10px]" >
+              <div className="flex items-center bg-[#FFFFFF] rounded-xl px-[20px]	 h-[100%]">
+                <img
+                  src={logo.toDataURL()}
+                  className="flex items-center w-[200px]"
+                />
+
+              </div>
+              <div className="flex flex-col ml-6">
+                <h1 className="text-5xl text-white font-bold m-0">
+                  <span className="text-blue-500">{"Infomoney"}</span>
+                </h1>
+                <p className="text-gray-300 text-3xl m-0">{"Finanças"}</p>
+              </div>
             </div>
           </div>
-          <div
-            className={`flex`}
-
-          >
-            {/* <img
-            src={post.toDataURL()}
-            className="flex h-[100px] w-[100px] rounded-full"
-          />
-          <div className="flex flex-col ml-6">
-            <h1 className="text-5xl text-white font-bold m-0">
-              {type === "welcome" ? "Welcome" : "Olocooo"},{" "}
-              <span className="text-blue-500">{displayName}!</span>
-            </h1>
-            <p className="text-gray-300 text-3xl m-0">{message}</p>
-          </div> */}
-          </div>
         </div>
-        
+
       </div>
     );
   }
